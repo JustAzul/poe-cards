da2ca3424948ec42858b13c62fa1640d0e042343
Azul <diego.kfu@hotmail.com>
2021-02-04 10:27:47 -0300

    Added Transition

--- pages/_app.js
+++ pages/_app.js
@@ -6,11 +6,12 @@ import '../styles/globals.css';
 import { CookiesProvider } from "react-cookie";
 import {useEffect, useState} from 'react';
 import useSocket from "../hooks/useSocket";
-import router from 'next/router';
+
+import { motion } from 'framer-motion';
 
 import Loader from '../components/Loader';
 
-function MyApp({ Component, pageProps }) {  
+function MyApp({ Component, pageProps, router }) {  
   const SocketIO = useSocket();  
 
   const [isLoading, setLoading] = useState(false);
@@ -35,25 +36,36 @@ function MyApp({ Component, pageProps }) {
   }, [SocketIO]);
 
   useEffect(() => {
-    const handleStart = (url) => (url !== router.asPath) && setLoading(true);
-    const handleComplete = (url) => (url === router.asPath) && setLoading(false);
+    const handleStart = url => (url !== router.asPath) && setLoading(true);
+    const handleComplete = url => (url === router.asPath) && setLoading(false);
 
     router.events.on('routeChangeStart', handleStart);
     router.events.on('routeChangeComplete', handleComplete);
     router.events.on('routeChangeError', handleComplete);
-
+    
     return () => {
-        try {
-          router.events.off('routeChangeStart', handleStart);
-          router.events.off('routeChangeComplete', handleComplete);
-          router.events.off('routeChangeError', handleComplete);
-        } catch (e) {}
+      try {
+        router.events.off('routeChangeStart', handleStart);
+        router.events.off('routeChangeComplete', handleComplete);
+        router.events.off('routeChangeError', handleComplete);
+      } catch (e) {}
     }
 });
 
+  const AnimationVariants = {
+    pageInitial: {
+      opacity: 0
+    },
+    pageAnimate: {
+      opacity: 1
+    }
+  };
+
 return (
     <CookiesProvider>
-      {isLoading ? <Loader /> : <Component SocketIO={SocketIO} isSocketConnected={isSocketConnected} {...pageProps} />}
+      <motion.div key={router['route']} initial="pageInitial" animate="pageAnimate" variants={AnimationVariants}>
+        {isLoading ? <Loader /> : <Component SocketIO={SocketIO} isSocketConnected={isSocketConnected} {...pageProps} />}
+      </motion.div>      
     </CookiesProvider>
   );
 }
