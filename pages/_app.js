83eadcdcd243ef3c0dfa7b8a45e917d7b8a3add5
Azul <diego.kfu@hotmail.com>
2021-02-05 05:07:21 -0300

    motion

--- pages/_app.js
+++ pages/_app.js
@@ -7,7 +7,7 @@ import { CookiesProvider } from "react-cookie";
 import {useEffect, useState} from 'react';
 import useSocket from "../hooks/useSocket";
 
-//import { motion } from 'framer-motion';
+import { motion } from 'framer-motion';
 import Loader from '../components/Loader';
 
 function MyApp({ Component, pageProps, router }) {  
@@ -51,9 +51,17 @@ function MyApp({ Component, pageProps, router }) {
     }
   }, [router.asPath]);
 
+  const start = {
+    opacity: 0
+  }
+
+  const end = {
+    opacity: 1
+  }
+
   return (
     <>
-      {isLoading ? <Loader /> : <CookiesProvider> <Component SocketIO={SocketIO} isSocketConnected={isSocketConnected} {...pageProps} /> </CookiesProvider>}
+      {isLoading ? <Loader /> : <CookiesProvider> <motion.div initial={start} animate={end} exit={start}> <Component SocketIO={SocketIO} isSocketConnected={isSocketConnected} {...pageProps} /> </motion.div> </CookiesProvider>}
     </>
     );
 }
