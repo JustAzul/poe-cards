25d5a274ed4a121cbd4475b0d6aaba0ab69ed1e6
Azul <diego.kfu@hotmail.com>
2021-02-05 19:26:17 -0300

    Removed motion from spinner

--- components/Spinner/index.js
+++ components/Spinner/index.js
@@ -1,21 +1,5 @@
 import styles from './index.module.css';
 
-import Transition from '../Transition';
-
 export default function Spinner() {
-    const transition = {
-        duration: 0.5
-    };
-
-    const start = {
-        opacity: 0,
-        transition
-    }
-
-    const end = {
-        opacity: 1,
-        transition
-    };
-
-    return <Transition styles={styles['loading']} />;
+    return <div styles={styles['loading']} />;
 }
