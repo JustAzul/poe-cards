69e94973568457607b51c435453a5f6dacf02391
Azul <diego.kfu@hotmail.com>
2021-02-05 05:31:26 -0300

    .

--- components/Spinner/index.js
+++ components/Spinner/index.js
@@ -1,5 +1,21 @@
 import styles from './index.module.css';
 
+import { motion } from 'framer-motion';
+
 export default function Spinner() {
-    return <div className={styles['loading']} />;
+    const transition = {
+        duration: 0.5
+    };
+
+    const start = {
+        opacity: 0,
+        transition
+    }
+
+    const end = {
+        opacity: 1,
+        transition
+    };
+
+    return <motion.div initial={start} animate={end} className={styles['loading']} />;
 }
