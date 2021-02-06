c48859793645476b52f5130e9dfcc8fd19a33d76
Azul <diego.kfu@hotmail.com>
2021-02-06 04:56:39 -0300

    Beautify

--- components/Layout/PlayerDonate/TwitchPlayer/index.js
+++ components/Layout/PlayerDonate/TwitchPlayer/index.js
@@ -7,9 +7,7 @@ function TwitchPlayer({ parent, ChannelName }) {
 
     const SRC = `https://player.twitch.tv/?channel=${ChannelName}&autoplay=${autoplay}&muted=${muted}&parent=${parent}`;
 
-    return (
-        <iframe className={styles.player} src={SRC} />
-    );
+    return <iframe className={styles.player} src={SRC} />;
 }
 
 export default TwitchPlayer;
