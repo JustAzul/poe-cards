c502929aee25fc92643dfea26756fc40d8120932
Azul <diego.kfu@hotmail.com>
2021-12-17 07:31:28 -0300

    gtag

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -7,14 +7,26 @@ import '../styles/main.css';
 import '../styles/globals.css';
 
 import type { AppProps } from 'next/app';
-
 import { CookiesProvider } from 'react-cookie';
 import { useEffect, useState } from 'react';
+
+import Script from 'next/script';
+import * as gtag from '../lib/gtag';
 
 import Loader from '../components/Loader';
 
 function MyApp({ Component, pageProps, router }: AppProps) {
   const [isLoading, setLoading] = useState<Boolean>(false);
+
+  useEffect(() => {
+    const handleRouteChange = (url: any) => {
+      gtag.pageview(url);
+    };
+    router.events.on('routeChangeComplete', handleRouteChange);
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange);
+    };
+  }, [router.events]);
 
   useEffect(() => {
     const handleStart = (url: string) => (url !== router.asPath) && setLoading(true);
@@ -39,9 +51,30 @@ function MyApp({ Component, pageProps, router }: AppProps) {
   if (isLoading) return <Loader />;
 
   return (
-    <CookiesProvider>
+    <>
+      {/* Global Site Tag (gtag.js) - Google Analytics */}
+      <Script
+        strategy="afterInteractive"
+        src={`https://www.googletagmanager.com/gtag/js?id=${gtag.GA_TRACKING_ID}`}
+      />
+      <Script
+        id="gtag-init"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${gtag.GA_TRACKING_ID}', {
+              page_path: window.location.pathname,
+            });
+          `,
+        }}
+      />
+      <CookiesProvider>
         <Component {...pageProps} />
       </CookiesProvider>
+    </>
   );
 }
 
