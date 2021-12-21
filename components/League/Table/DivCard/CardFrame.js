64beb22008d64488e2febcfdc48a160bf2d51fe1
Azul <diego.kfu@hotmail.com>
2021-12-21 14:03:00 -0300

    memo + blur

--- components/League/Table/DivCard/CardFrame.js
+++ components/League/Table/DivCard/CardFrame.js
@@ -1,18 +1,20 @@
 import Image from 'next/image';
+import { memo } from 'react';
 import styles from './index.module.css';
 
 import CardBG from '../../../../public/images/cardbg.png';
 
-export default function CardFrame() {
+function CardFrame() {
   return (
         <span className={styles['divicard-frame']}>
             <Image
-                // placeholder="blur"
+                placeholder="blur"
                 src={CardBG}
                 width={220}
                 height={334}
-                priority={true}
             />
         </span>
   );
 }
+
+export default memo(CardFrame);
