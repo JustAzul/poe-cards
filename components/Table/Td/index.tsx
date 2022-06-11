02a895f4fe0a5e9b992099a6097f7e9ff3e07632
Azul <diego.kfu@hotmail.com>
2022-06-11 12:20:37 -0300

    changed structure to better maintance

--- components/Table/Td/index.tsx
+++ components/Table/Td/index.tsx
@@ -1,8 +1,8 @@
-import Link from 'next/link';
 import { ReactNode, memo } from 'react';
-import styles from '../index.module.css';
 
 import type { KeyStates } from '../../../hooks/interfaces';
+import Link from 'next/link';
+import styles from '../index.module.css';
 
 interface Props {
     children: ReactNode,
@@ -21,7 +21,7 @@ function Td({
   // eslint-disable-next-line no-shadow
   children, SetMouseOver, KeyState, _Key, Href, Class, setTitle, Click, SearchString, SearchMaxValue,
 }: Props) {
-  const TD = (_children: ReactNode) => (<td onClick={() => Click && Click(SearchString, SearchMaxValue)} title={setTitle} onMouseOver={() => SetMouseOver(_Key)} onMouseLeave={() => SetMouseOver('')} className={`${Class ? `${Class} ` : ''}${styles.column100}${_Key === 'c1' ? ` ${styles.column1}` : ''}${_Key === KeyState ? ` ${styles['hov-column-ver1']}` : ''}`}>{_children}</td>);
+  const TD = (_children: ReactNode) => (<td onClick={() => Click && Click({ itemName: SearchString, searchMaxValue: SearchMaxValue })} title={setTitle} onMouseOver={() => SetMouseOver(_Key)} onMouseLeave={() => SetMouseOver('')} className={`${Class ? `${Class} ` : ''}${styles.column100}${_Key === 'c1' ? ` ${styles.column1}` : ''}${_Key === KeyState ? ` ${styles['hov-column-ver1']}` : ''}`}>{_children}</td>);
 
   if (Href) {
     return (
