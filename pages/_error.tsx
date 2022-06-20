328ebff4f9aa86aba167f3c6b583224d178a1956
Azul <diego.kfu@hotmail.com>
2022-06-20 08:36:49 -0300

    Slightly refactor

--- pages/_error.tsx
+++ pages/_error.tsx
@@ -12,9 +12,13 @@ function Error({ statusCode, leagueError = false }: Props) {
     return (
       <div className={`${styles['notfound-2']} user-select-none`}>
         <div className={styles.notfound}>
-          <div className={styles['notfound-404']}></div>
-          <h1>404</h1>
-          <h2>Oops! {leagueError ? 'League' : 'Page'} Not Found</h2>
+          <div className={styles['notfound-404']} />
+          <h1>
+            404
+          </h1>
+          <h2>
+            Oops! {leagueError ? 'League' : 'Page'} Not Found
+          </h2>
           <p>
             Sorry but the {leagueError ? 'league' : 'page'} you are looking for does not exist, have been removed. name changed or is temporarily unavailable
           </p>
