47bdb66eafe2e0c797928d91e326d646518e113f
Azul <diego.kfu@hotmail.com>
2022-07-08 16:17:29 -0300

    env fix

--- firebase/adminApp.js
+++ firebase/adminApp.js
@@ -3,8 +3,8 @@ import * as firebaseAdmin from 'firebase-admin';
 if (!firebaseAdmin.apps.length) {
   firebaseAdmin.initializeApp({
     credential: firebaseAdmin.credential.cert({
-      privateKey: Buffer.from(process.env.NEXT_PUBLIC_FIREBASE_PRIVATE_KEY, 'base64').toString('ascii'),
-      clientEmail: process.env.NEXT_PUBLIC_FIREBASE_CLIENT_EMAIL,
+      privateKey: Buffer.from(process.env.FIREBASE_PRIVATE_KEY, 'base64').toString('ascii'),
+      clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
       projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
     }),
     databaseURL: `https://${process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID}.firebaseio.com`,
